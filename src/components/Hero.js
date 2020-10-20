76118db1500dc3dd5856f71c743476dfb446d01b
Michelle Tanzil <mtanzil01@gmail.com>
2020-10-19 19:12:07 -0700

    Set up hero and services components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,44 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
+
+const heroQuery = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(heroQuery)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"> </div>
+            <h1>i'm michelle</h1>
+            {/* TODO: Typing effect for titles */}
+            <h4>Software Developer</h4>
+            <Link to="/about" className="btn">
+              About Me
+            </Link>
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" alt={"Michelle Tanzil"} />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
